97001f02cbb834332475713b2298dd9308eefeee
Екатерина Носенко <nosenkoek@gmail.com>
2022-10-31 20:07:47 +0300

    created database and docker-compose

---
+++ education_admin/config/components/database.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        # 'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', 5432),
+        'OPTIONS': {
+            'options': '-c search_path=public,content'
+        }
+    }
+}
---
+++ education_admin/config/components/internationalization.py
@@ -0,0 +1,10 @@
+import os.path
+from django.apps import apps
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+
+USE_L10N = True
